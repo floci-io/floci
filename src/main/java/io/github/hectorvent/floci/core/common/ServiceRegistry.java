4069f9978b4761b77057430d1701c74c54b80252
Hector Ventura <hectorvent@gmail.com>
2026-04-04 22:30:18 -0500

    feat(ec2): adding ec2 service

--- src/main/java/io/github/hectorvent/floci/core/common/ServiceRegistry.java
+++ src/main/java/io/github/hectorvent/floci/core/common/ServiceRegistry.java
@@ -50,6 +50,7 @@ public class ServiceRegistry {
             case "acm" -> config.services().acm().enabled();
             case "email" -> config.services().ses().enabled();
             case "es" -> config.services().opensearch().enabled();
+            case "ec2" -> config.services().ec2().enabled();
             default -> true;
         };
     }
@@ -79,6 +80,7 @@ public class ServiceRegistry {
         if (config.services().acm().enabled()) enabled.add("acm");
         if (config.services().ses().enabled()) enabled.add("email");
         if (config.services().opensearch().enabled()) enabled.add("es");
+        if (config.services().ec2().enabled()) enabled.add("ec2");
         return enabled;
     }
 
@@ -110,6 +112,7 @@ public class ServiceRegistry {
         services.put("acm", status(config.services().acm().enabled()));
         services.put("email", status(config.services().ses().enabled()));
         services.put("es", status(config.services().opensearch().enabled()));
+        services.put("ec2", status(config.services().ec2().enabled()));
         return services;
     }
 
