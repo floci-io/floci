55ec31d310e53fed0c112ac213b0b908c8810d00
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2026-08-02 23:09:58 +0100

    test(ec2): cover blank VpcId for CreateSubnet over the wire
    Companion to the unit-level blank VpcId case, exercised through the
    Query API handler.

--- src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
+++ src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
@@ -687,6 +687,22 @@ class Ec2IntegrationTest {
     }
 
     @Test
+    @Order(20)
+    void createSubnetWithBlankVpcIdReturnsMissingParameter() {
+        given()
+            .formParam("Action", "CreateSubnet")
+            .formParam("VpcId", "   ")
+            .formParam("CidrBlock", "10.0.2.0/24")
+            .header("Authorization", AUTH_HEADER)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(400)
+            .body("Response.Errors.Error.Code", equalTo("MissingParameter"))
+            .body("Response.Errors.Error.Message", equalTo("The request must contain the parameter VpcId"));
+    }
+
+    @Test
     @Order(21)
     void describeSubnetById() {
         given()
