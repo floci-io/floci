deeb6607347d2c2cf23067dcadf5fc6dcb2ffdd5
NAMAN JAIN <naman.049259@tmu.ac.in>
2026-08-09 15:07:06 +0530

    feat(cloudhsmv2): implement CloudHSM v2 service

--- src/main/java/io/github/hectorvent/floci/config/EmulatorConfig.java
+++ src/main/java/io/github/hectorvent/floci/config/EmulatorConfig.java
@@ -595,6 +595,7 @@ public interface EmulatorConfig {
         S3VectorsServiceConfig s3vectors();
         IotServiceConfig iot();
         IotDataServiceConfig iotdata();
+        CloudHsmV2ServiceConfig cloudhsmv2();
         RumServiceConfig rum();
     }
 
@@ -1070,6 +1071,11 @@ public interface EmulatorConfig {
         /** Seconds to wait before transitioning from PENDING_VALIDATION to ISSUED (0 = immediate) */
         @WithDefault("0")
         int validationWaitSeconds();
+    }
+
+    interface CloudHsmV2ServiceConfig {
+        @WithDefault("true")
+        boolean enabled();
     }
 
     interface AthenaServiceConfig {
