7b28818c8e0c0c240c53511e8f659a9a03638f35
Vu The Huy <vuthehuy.t61@gmail.com>
2026-08-22 20:15:54 +0700

    feat: add redshift domain models

---
+++ src/main/java/io/github/hectorvent/floci/services/redshift/model/Cluster.java
@@ -0,0 +1,23 @@
+package io.github.hectorvent.floci.services.redshift.model;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class Cluster {
+    private String clusterIdentifier;
+    private String nodeType;
+    private String masterUsername;
+    private String clusterStatus;
+    private Endpoint endpoint;
+
+    public String getClusterIdentifier() { return clusterIdentifier; }
+    public void setClusterIdentifier(String clusterIdentifier) { this.clusterIdentifier = clusterIdentifier; }
+    public String getNodeType() { return nodeType; }
+    public void setNodeType(String nodeType) { this.nodeType = nodeType; }
+    public String getMasterUsername() { return masterUsername; }
+    public void setMasterUsername(String masterUsername) { this.masterUsername = masterUsername; }
+    public String getClusterStatus() { return clusterStatus; }
+    public void setClusterStatus(String clusterStatus) { this.clusterStatus = clusterStatus; }
+    public Endpoint getEndpoint() { return endpoint; }
+    public void setEndpoint(Endpoint endpoint) { this.endpoint = endpoint; }
+}
