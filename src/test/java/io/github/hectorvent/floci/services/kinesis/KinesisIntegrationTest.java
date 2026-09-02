b095ddbf38b392225bd52dd2b04a40375e024246
Niels Kootstra <545768+nkootstra@users.noreply.github.com>
2026-09-02 14:50:07 +0200

    fix(kinesis): handle minimum partition key hash

--- src/test/java/io/github/hectorvent/floci/services/kinesis/KinesisIntegrationTest.java
+++ src/test/java/io/github/hectorvent/floci/services/kinesis/KinesisIntegrationTest.java
@@ -765,6 +765,29 @@ class KinesisIntegrationTest {
             .body("Records.size()", equalTo(0));
     }
 
+    @Test
+    @Order(43)
+    void putRecordAcceptsPartitionKeyWithMinimumHashCode() {
+        given()
+            .header("X-Amz-Target", "Kinesis_20131202.CreateStream")
+            .contentType(KINESIS_CONTENT_TYPE)
+            .body("""
+                {"StreamName": "minimum-hash-partition-key-test", "ShardCount": 3}
+                """)
+        .when().post("/").then().statusCode(200);
+
+        given()
+            .header("X-Amz-Target", "Kinesis_20131202.PutRecord")
+            .contentType(KINESIS_CONTENT_TYPE)
+            .body("""
+                {"StreamName": "minimum-hash-partition-key-test", "Data": "dGVzdA==", "PartitionKey": "polygenelubricants"}
+                """)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(200);
+    }
+
     // --- AT_TIMESTAMP iterator coverage ---
 
     private String atTimestampCreateStream(String name) {
